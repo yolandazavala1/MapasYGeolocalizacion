82758c0dcc94b9372a7dd35aad2e723e4bc3aae4
Yolanda Zavala <s16120276@alumnos.itsur.edu.mx>
2021-03-23 23:31:07 -0600

    segundo commit

--- app/src/main/java/com/example/mapasygeolocalizacion/MainActivity.java
+++ app/src/main/java/com/example/mapasygeolocalizacion/MainActivity.java
@@ -1,26 +1,65 @@
 package com.example.mapasygeolocalizacion;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity {
-    private EditText txtLatitudActual,txtLongitudActual,txtLatitudDestino,txtLongitudDestino;
+public class MainActivity extends AppCompatActivity{
+    private EditText txtLatitudActual,txtLongitudActual, txtLatitudDestino,txtLongitudDestino;
     private Button btnOK;
+    public static Intent intent = new Intent();
+    private FusedLocationProviderClient fusedLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         btnOK=findViewById(R.id.btnRuta);
         txtLatitudActual=findViewById(R.id.txtLatitudActual);
         txtLongitudActual=findViewById(R.id.txtLongitudActual);
         txtLatitudDestino=findViewById(R.id.txtLatitudDestino);
-        txtLongitudDestino=findViewById(R.id.txtLongitudDestino);
+        txtLongitudDestino=findViewById(R.id.txtLatitudDestino);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+
+                            txtLatitudActual.setText(String.valueOf(location.getLatitude()));
+                            txtLongitudActual.setText(String.valueOf(location.getLongitude()));
+                        }else {
+                            Log.i("MiUltUbi", "No hay ubicaciòn, verifique si conexión y gps");}
+
+                    }
+                });
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Origen
+                intent.putExtra("LatitudCasa", txtLatitudDestino.getText().toString());
+                intent.putExtra("LongitudCasa",txtLongitudDestino.getText().toString());
+
+                //Destino
+                intent.putExtra("latituActual",txtLatitudActual.getText().toString());
+                intent.putExtra("longitudActual",txtLongitudActual.getText().toString());
+
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/mapasygeolocalizacion/MapsActivity.java
+++ app/src/main/java/com/example/mapasygeolocalizacion/MapsActivity.java
@@ -1,15 +1,31 @@
 package com.example.mapasygeolocalizacion;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.os.Bundle;
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -25,22 +41,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
+    //Origen
+    double  latitudCasa= Double.parseDouble(MainActivity.intent.getExtras().getString("LatitudCasa"));
+    double  longitudCasa = Double.parseDouble(MainActivity.intent.getExtras().getString("LongitudCasa"));
+
+    //Destino
+    double latitudActual = Double.parseDouble(MainActivity.intent.getExtras().getString("latituActual"));
+    double longitudActual = Double.parseDouble(MainActivity.intent.getExtras().getString("longitudActual"));
+
+    //Key
+    String key = "AIzaSyDZx8ah9aVIZ57h7k1-V2DnivCSKNkLtXA";
+
+    JSONObject jso;
+
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng ubicacion = new LatLng(latitudActual, longitudActual);
+        LatLng destino = new LatLng(latitudCasa, longitudCasa);
+        mMap.addMarker(new MarkerOptions().position(ubicacion).title("Ubicacion actual"));
+        mMap.addMarker(new MarkerOptions().position(destino).title("Ubicacion destino"));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder().target(ubicacion).zoom(14).bearing(30).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        String url = "https://maps.googleapis.com/maps/api/directions/json?origin="+latitudActual+","+longitudActual+"&destination="+latitudCasa+","+longitudCasa+"&key="+key+"";
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                try {
+                    jso = new JSONObject(response);
+                    trasaruta(jso);
+                    Log.i("JsonRuta:", ""+response);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(stringRequest);
+
     }
+
+    private void trasaruta(JSONObject jso) {
+        JSONArray jRoutes;
+        JSONArray jLegs;
+        JSONArray jSteps;
+        try {
+            jRoutes = jso.getJSONArray("routes");
+            for (int i=0; i<jRoutes.length();i++){
+
+                jLegs = ((JSONObject)jRoutes.get(i)).getJSONArray("legs");
+
+                for (int j=0; j<jLegs.length(); j++){
+
+                    jSteps = ((JSONObject)jLegs.get(j)).getJSONArray("steps");
+
+                    for (int k=0; k<jSteps.length(); k++){
+                        String polyline = ""+((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
+
+                        Log.i("end",""+polyline);
+                        List<LatLng> list = PolyUtil.decode(polyline);
+                        mMap.addPolyline(new PolylineOptions().addAll(list).color(Color.BLUE).width(5));
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
